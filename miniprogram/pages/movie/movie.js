e65886ae56057cd75f53f1dbd7c8dbd19e2da255
yuan1 <limingyuan1996@gmail.com>
2020-03-08 20:28:03 +0800

    update user page

--- miniprogram/pages/movie/movie.js
+++ miniprogram/pages/movie/movie.js
@@ -5,7 +5,8 @@ Page({
    * 页面的初始数据
    */
   data: {
-    movies: []
+    movies: [],
+    floorstatus: false
   },
   getMovies: function() {
     wx.showLoading({
@@ -26,10 +27,24 @@ Page({
       console.error(err)
     })
   },
-  gotoComment(event){
+  gotoComment(event) {
     wx.navigateTo({
       url: `../comment/comment?movieid=${event.target.dataset.movieid}&movietitle=${event.target.dataset.movietitle}&moviecover=${event.target.dataset.moviecover}`,
     })
+  },
+  //回到顶部 
+  // 一键回到顶部
+  goTop: function(e) {
+    if (wx.pageScrollTo) {
+      wx.pageScrollTo({
+        scrollTop: 0
+      })
+    } else {
+      wx.showModal({
+        title: '提示',
+        content: '当前微信版本过低，无法使用该功能，请升级到最新微信版本后重试。'
+      })
+    }
   },
   /**
    * 生命周期函数--监听页面加载
@@ -85,5 +100,17 @@ Page({
    */
   onShareAppMessage: function() {
 
+  },
+  // 获取滚动条当前位置
+  onPageScroll: function(e) {
+    if (e.scrollTop > 100) {
+      this.setData({
+        floorstatus: true
+      });
+    } else {
+      this.setData({
+        floorstatus: false
+      });
+    }
   }
 })
